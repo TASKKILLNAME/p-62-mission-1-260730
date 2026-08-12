26d69391508d3a0137934e5c9bd419fca41c9c65
TASKKILLNAME <dongjunjwa4020@naver.com>
2026-08-12 17:22:27 +0900

    스프링부트 프로젝트 생성 & @ResponseBody로 출력

---
+++ src/main/java/com/back/practicejsb/PracticeJsbApplication.java
@@ -0,0 +1,13 @@
+package com.back.practicejsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeJsbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeJsbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/back/practicejsb/PracticeJsbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.practicejsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeJsbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
