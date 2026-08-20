8ec78bcef77fdf591528973dd1522e8af0df8010
TASKKILLNAME <dongjunjwa4020@naver.com>
2026-08-20 18:01:57 +0900

    페이징 기능 추가하기

--- src/test/java/com/back/practicejsb/PracticeJsbApplicationTests.java
+++ src/test/java/com/back/practicejsb/PracticeJsbApplicationTests.java
@@ -1,19 +1,11 @@
 package com.back.practicejsb;
 
-import com.back.practicejsb.domain.post.answer.entity.Answer;
 import com.back.practicejsb.domain.post.answer.repository.AnswerRepository;
-import com.back.practicejsb.domain.post.question.entity.Question;
 import com.back.practicejsb.domain.post.question.repository.QuestionRepository;
+import com.back.practicejsb.domain.post.question.service.QuestionService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class PracticeJsbApplicationTests {
@@ -25,99 +17,15 @@ class PracticeJsbApplicationTests {
 
     @Autowired
     AnswerRepository answerRepository;
+    @Autowired
+    private QuestionService questionService;
 
     @Test
-    void testJpa1() {
-        Question q1 = new Question("sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
-        questionRepository.save(q1);
-
-        Question q2 = new Question("스프링부트 모델 질문입니다.", "id는 자동으로 생성 되나요?");
-        questionRepository.save(q2);
-    }
-
-    @Test
-    void testJpa2() {
-        List<Question> all = questionRepository.findAll();
-        assertEquals(2, all.size());
-
-        Question q = all.get(0);
-        assertEquals("sbb가 무엇인가요?", q.getSubject());
-    }
-
-    @Test
-    void testJpa3() {
-        Optional<Question> oq = questionRepository.findById(1);
-        if(oq.isPresent()) {
-            Question q = oq.get();
-            assertEquals("sbb가 무엇인가요?", q.getSubject());
+    void testJpa() {
+        for(int i = 1; i <= 300; i++) {
+            String subject = String.format("테스트 데이터입니다:[%03d]", i);
+            String content  = "내용무";
+            questionService.create(subject, content);
         }
-    }
-
-    @Test
-    void testJpa4() {
-        Question q = questionRepository.findBySubject("sbb가 무엇인가요?");
-        assertEquals(1, q.getId());
-    }
-
-    @Test
-    void testJpa5() {
-        Question q = questionRepository.findBySubjectAndContent("sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
-        assertEquals(1, q.getId());
-    }
-
-    @Test
-    void testJpa6() {
-        List<Question> qList = questionRepository.findBySubjectLike("sbb%");
-        Question q = qList.get(0);
-        assertEquals("sbb가 무엇인가요?", q.getSubject());
-    }
-
-    @Test
-    void testJpa7() {
-        Optional<Question> oq = questionRepository.findById(1);
-        assertTrue(oq.isPresent());
-        Question q = oq.get();
-        // 엔티티에서 직접 Setter를 사용하지 않기때문에 update 메소드 생성 후 사용
-    }
-
-    @Test
-    void testJpa8() {
-        assertEquals(2, questionRepository.count());
-        Optional<Question> oq = questionRepository.findById(1);
-        assertTrue(oq.isPresent());
-        Question q = oq.get();
-        questionRepository.delete(q);
-        assertEquals(1, questionRepository.count());
-    }
-
-    @Test
-    void testJpa9() {
-        Optional<Question> oq = questionRepository.findById(2);
-        assertTrue(oq.isPresent());
-        Question q = oq.get();
-
-        Answer a = new Answer("네 자동으로 생성됩니다.", q);
-        answerRepository.save(a);
-    }
-
-    @Test
-    void testJpa10() {
-        Optional<Answer> oa = answerRepository.findById(1);
-        assertTrue(oa.isPresent());
-        Answer a = oa.get();
-        assertEquals(2, a.getQuestion().getId());
-    }
-
-    @Transactional
-    @Test
-    void testJpa11() {
-        Optional<Question> oq = questionRepository.findById(2);
-        assertTrue(oq.isPresent());
-        Question q = oq.get();
-
-        List<Answer> answerList = q.getAnswerList();
-
-        assertEquals(1, answerList.size());
-        assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
     }
 }
